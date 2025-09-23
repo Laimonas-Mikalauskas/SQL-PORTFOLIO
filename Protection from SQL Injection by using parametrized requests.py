dd8d6f40932be1c26ea0ac56f8356f14c53d290f
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-09-23 12:47:03 +0300

    Update Protection from SQL Injection by using parametrized requests.py

--- Protection from SQL Injection by using parametrized requests.py
+++ Protection from SQL Injection by using parametrized requests.py
@@ -2,14 +2,14 @@ from this import c
 
 from SQLite.main import conn
 
-inp_username = input("Įveskite username: ")
-inp_password = input("Įveskite slaptažodį: ")
+inp_username = input("Enter the username: ")
+inp_password = input("Enter the password: ")
 
 with conn:
     c.execute("SELECT * FROM user WHERE user.username=? AND user.password=?;", (inp_username, inp_password))
     res = c.fetchall()
     if res:
-        print("Jūsų profilio duomenys yra:")
+        print("Your profile data is:")
         print(res)
     else:
-        print(f"Vartotojas {inp_username} neegzistuoja arba neteisingas slaptažodis")
+        print(f"User {inp_username} doesn't exist or  incorrect password ")
